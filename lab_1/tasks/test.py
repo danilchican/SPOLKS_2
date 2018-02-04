df0ff1f36b725b1ad5f6e401cdf67004cb64ec61
danilchican <danilchican@mail.ru>
2018-02-05 00:36:06 +0300

    Modified test

--- lab_1/tasks/test.py
+++ lab_1/tasks/test.py
@@ -3,8 +3,11 @@ import socket
 from builder.package_builder import PackageBuilder
 from ip.ip_header import IPHeader, DEFAULT_TTL
 from ip.ipv4 import PROTOCOL_VERSION
+from request.request import Request
 
 if __name__ == '__main__':  # TODO test remove
+    request = Request('ya.ru')
+
     src = socket.inet_aton('192.168.0.1')
     dest = socket.inet_aton('192.168.0.2')
 
