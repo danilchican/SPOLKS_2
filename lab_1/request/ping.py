d0a6744ff960ff5a21524d19f93af5051de2125a
danilchican <danilchican@mail.ru>
2018-02-06 19:55:42 +0300

    Created _send() method for ping

--- lab_1/request/ping.py
+++ lab_1/request/ping.py
@@ -1,10 +1,12 @@
 import socket
 import time
 
+from builder.icmp_builder import ICMPBuilder
 from request.request import Request
 
 MILLISECONDS_PRECISION = 1000
 SLEEP_TIME_IN_SEC = 1
+DEFAULT_PORT = 1
 
 
 class PingRequest(Request):
@@ -20,7 +22,7 @@ class PingRequest(Request):
                     print("Request timed out for {}".format(self.host_addr))
                 else:
                     latency *= MILLISECONDS_PRECISION
-                    print("Reply from {}: bytes={} icmp_seq={} TTL={} time={:.0f}ms "
+                    print("Reply from {}: bytes={} icmp_seq={} TTL={} time={:.0f}ms"
                           .format(host, size - len(ip), icmp["seq_num"], ip["ttl"], latency))
 
                 time.sleep(SLEEP_TIME_IN_SEC)
@@ -30,10 +32,19 @@ class PingRequest(Request):
 
     def _ping(self, packet_index):
         with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as sock:
-            send_time = self._send(sock, packet_index) # TODO
-            (recv_time, addr, ip, icmp, size) = self._receive(sock) # TODO
+            send_time = self._send(sock, packet_index)
+            (receive_time, addr, ip, icmp, size) = self._receive(sock)  # TODO
 
-        if recv_time is None:
+        if receive_time is None:
             return None, 0, 0, 0, 0
 
-        return recv_time - send_time, addr, ip, icmp, size
+        return receive_time - send_time, addr, ip, icmp, size
+
+    def _send(self, sock, packet_index):
+        packet = ICMPBuilder.build(packet_index, self.package_id, self.max_payload_size)
+
+        send_time = time.time()
+        sock.sendto(packet, (self.host_addr, DEFAULT_PORT))
+        self.sent += 1  # calculate statistics
+
+        return send_time
