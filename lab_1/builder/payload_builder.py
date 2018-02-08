0c9383de3e2015af01c49856b92f00f3d92fb735
danilchican <danilchican@mail.ru>
2018-02-08 19:38:19 +0300

    Modified ping request

--- lab_1/builder/payload_builder.py
+++ lab_1/builder/payload_builder.py
@@ -1,9 +1,8 @@
 import string
-from random import random
+import random
 
 import struct
-
-from helper.logger import get_logger
+# from helper.logger import get_logger
 from request.request import DEFAULT_MAX_PAYLOAD_SIZE
 
 
@@ -16,6 +15,6 @@ class PayloadBuilder:
         :param length: length of generated
         :return: package payload
         """
-        payload = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
-        get_logger(__file__).info("Payload generated: " + payload)
-        return struct.pack('d', payload).ljust(length, b'\x00')
+        payload = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+        # get_logger(__file__).info("Payload generated: " + payload)
+        return struct.pack('p', bytes(payload, 'utf-8')).ljust(length, b'\x00')
--- lab_1/request/ping.py
+++ lab_1/request/ping.py
@@ -1,18 +1,25 @@
 import socket
 import time
+import struct
+import select
 
 from builder.icmp_builder import ICMPBuilder
+from ip.ipv4 import AUTO_VALUE
 from request.request import Request
+from helper.packet_parser import PacketParser
 
 MILLISECONDS_PRECISION = 1000
 SLEEP_TIME_IN_SEC = 1
 DEFAULT_PORT = 1
+SIZE_BUFFER = 2048
+STATISTICS_INCREMENTER = 1
+FULL_PERCENTAGE = 100
 
 
 class PingRequest(Request):
     def run(self):
-        print("Ping host:" + self.host_name + ", ip: " + self.host_addr)
-        print("Data: " + self.max_payload_size)
+        print("Ping host: {}, ip: {}".format(self.host_name, self.host_addr))
+        print("Data: {}".format(self.max_payload_size))
 
         for packet in range(self.packets):
             try:
@@ -25,18 +32,19 @@ class PingRequest(Request):
                     print("Reply from {}: bytes={} icmp_seq={} TTL={} time={:.0f}ms"
                           .format(host, size - len(ip), icmp["seq_num"], ip["ttl"], latency))
 
-                time.sleep(SLEEP_TIME_IN_SEC)
             except socket.gaierror as errno:
                 print("Socket error: {}".format(errno))
                 break
 
+            time.sleep(SLEEP_TIME_IN_SEC)
+
     def _ping(self, packet_index):
         with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as sock:
             send_time = self._send(sock, packet_index)
-            (receive_time, addr, ip, icmp, size) = self._receive(sock)  # TODO
+            (receive_time, addr, ip, icmp, size) = self._receive(sock)
 
         if receive_time is None:
-            return None, 0, 0, 0, 0
+            return None, AUTO_VALUE, AUTO_VALUE, AUTO_VALUE, AUTO_VALUE
 
         return receive_time - send_time, addr, ip, icmp, size
 
@@ -45,6 +53,45 @@ class PingRequest(Request):
 
         send_time = time.time()
         sock.sendto(packet, (self.host_addr, DEFAULT_PORT))
-        self.sent += 1  # calculate statistics
-
+        self.sent += STATISTICS_INCREMENTER  # calculate statistics
         return send_time
+
+    def _receive(self, sock):
+        time_left = self.timeout
+
+        while True:
+            begin_time = time.time()
+            sockets = select.select([sock], [], [], time_left)
+            wait_time = time.time() - begin_time
+
+            if sockets[0] == []:
+                break
+
+            (packet_data, _) = sock.recvfrom(SIZE_BUFFER, socket.MSG_PEEK)
+            size = len(packet_data) - 18
+            ip_header, icmp_header = PacketParser.parse(packet_data, self.package_id)
+
+            if ip_header is not None:
+                (data, _) = sock.recvfrom(SIZE_BUFFER)
+                ip, icmp = PacketParser.parse(packet_data, self.package_id)
+
+                receive_time = time.time()
+                host_ip = socket.inet_ntoa(
+                    struct.pack("I", ip["src_ip"]))
+                self.received += STATISTICS_INCREMENTER
+                return receive_time, host_ip, ip, icmp, size
+
+            time_left = time_left - wait_time
+
+            if time_left <= AUTO_VALUE:
+                break
+
+        return None, AUTO_VALUE, AUTO_VALUE, AUTO_VALUE, AUTO_VALUE
+
+    def statistic(self):
+        lost = self.sent - self.received
+        lost_percent = (FULL_PERCENTAGE * lost) / self.sent
+
+        print("\nPing statistics for {}:".format(self.host_addr))
+        print("\tPackets: Sent = {}, Received = {}, Lost = {} ({:.0f}% loss)"
+              .format(self.sent, self.received, lost, lost_percent))
